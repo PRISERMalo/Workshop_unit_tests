dbc7454d54fc7743a9bcb304be3a76a543a69e44
PRISERMalo <malo.priser@epitech.eu>
2024-04-17 18:02:01 +0400

    toto

--- tests/tests_my_strndup.c
+++ tests/tests_my_strndup.c
@@ -9,13 +9,15 @@ Test(my_strndup, test_my_strndup_neg, .init=redirect, .timeout=1)
 {
     cr_assert(my_strndup("toto", -1) == NULL);
 }
-// Test(my_strndup, test_my_strndup_ternaire, .init=redirect, .timeout=1)
-// {
-//     char *str = malloc(sizeof(char) * 5);
-//     cr_assert(my_strndup(str, 3) == NULL);
-// }
+Test(my_strndup, test_my_strndup_ternaire, .init=redirect, .timeout=1)
+{
+    char *str = malloc(sizeof(char) * 5);
+    str = "toto";
+    cr_assert(my_strndup(str, 3) != NULL);
+}
 Test(my_strndup, test_my_strndup_ternaire2, .init=redirect, .timeout=1)
 {
     char *str = malloc(sizeof(char) * 5);
-    cr_assert(my_strndup(str, 5) == NULL);
+    str = "toto";
+    cr_assert(my_strndup(str, 5) != NULL);
 }
